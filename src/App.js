f1cdb2122a3285c4aff515b29dde8bff4f8e910c
Hiten1708 <68720970+Hiten1708@users.noreply.github.com>
2022-05-15 03:01:03 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './Components/SignUp/SignUp';
+import NavSection from './Components/NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <SignUp/>  
+      <SignUp/>
     </div>
   );
 }
